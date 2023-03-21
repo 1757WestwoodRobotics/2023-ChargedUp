2636af7763e9d9b8ff5e59107fbdedc2fb93ecd8
Luke Maxwell <24lbmaxwell@protonmail.com>
2023-03-21 19:45:14 -0400

    lights but with PWM port

--- subsystems/lightsubsystem.py
+++ subsystems/lightsubsystem.py
@@ -2,7 +2,8 @@ from enum import Enum, auto
 from commands2 import SubsystemBase
 
 from ctre.led import CANdle, RainbowAnimation, ColorFlowAnimation
-from wpilib import RobotState
+
+from wpilib import AddressableLED, RobotState
 
 import constants
 
@@ -17,28 +18,41 @@ class LightSubsystem(SubsystemBase):
     def __init__(self) -> None:
         SubsystemBase.__init__(self)
         self.setName(__class__.__name__)
-        self.light = CANdle(constants.kCANdleID, constants.kCANivoreName)
+        self.light = AddressableLED(8)
 
-        self.disabledAnimation = RainbowAnimation(1, 0.5, 8)
-        self.cubeAnimation1 = ColorFlowAnimation(204, 0, 204, 255, 0.7, 17, ledOffset=8)  # purple
-        self.cubeAnimation2 = ColorFlowAnimation(204, 0, 204, 255, 0.7, 17, ledOffset=8+17)  # purple
-        self.coneAnimation1 = ColorFlowAnimation(255, 255, 0, 255, 0.7, 17, ledOffset=8)  # yellow
-        self.coneAnimation2 = ColorFlowAnimation(255, 255, 0, 255, 0.7, 17, ledOffset=8+ 17)  # yellow
+        self.ledBuffer = [AddressableLED.LEDData() for i in range(34)]
+        self.light.setLength(len(self.ledBuffer))
+
+        # self.disabledAnimation = RainbowAnimation(1, 0.5, 8)
+        # self.cubeAnimation1 = ColorFlowAnimation(204, 0, 204, 255, 0.7, 17, ledOffset=8)  # purple
+        # self.cubeAnimation2 = ColorFlowAnimation(204, 0, 204, 255, 0.7, 17, ledOffset=8+17)  # purple
+        # self.coneAnimation1 = ColorFlowAnimation(255, 255, 0, 255, 0.7, 17, ledOffset=8)  # yellow
+        # self.coneAnimation2 = ColorFlowAnimation(255, 255, 0, 255, 0.7, 17, ledOffset=8+ 17)  # yellow
 
         self.state = LightSubsystem.State.No
 
+        self.hueRotation = 0
+
+    def rainbow(self) -> None:
+        for i in range(34):
+            hue = self.hueRotation + (i * 180 / 34) % 180
+            self.ledBuffer[i].setHSV(int(hue), 255, 128)
+        self.hueRotation += 3
+        self.hueRotation %= 180
     def periodic(self) -> None:
         if RobotState.isDisabled():
-            self.light.animate(self.disabledAnimation)
+            self.rainbow()
         else:
             if self.state == LightSubsystem.State.No:
-                self.light.animate(self.disabledAnimation)
+                self.rainbow()
             elif self.state == LightSubsystem.State.Cone:
-                self.light.animate(self.coneAnimation1)
-                self.light.animate(self.coneAnimation2, 1)
+                for led in self.ledBuffer:
+                    led.setRGB(255,255,0)
             elif self.state == LightSubsystem.State.Cube:
-                self.light.animate(self.cubeAnimation1)
-                self.light.animate(self.cubeAnimation2, 1)
+                for led in self.ledBuffer:
+                    led.setRGB(204,0,204)
+
+        self.light.setData(self.ledBuffer)
 
     def offLights(self) -> None:
         self.state = LightSubsystem.State.No
