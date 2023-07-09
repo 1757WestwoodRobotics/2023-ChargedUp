fc10785485325b2873ed14761664c8c17e975ae2
Luke Maxwell <24lbmaxwell@protonmail.com>
2023-07-09 16:31:05 -0400

    better light anims

--- subsystems/lightsubsystem.py
+++ subsystems/lightsubsystem.py
@@ -1,7 +1,7 @@
 from enum import Enum, auto
 from commands2 import SubsystemBase
 
-from ctre.led import CANdle, RainbowAnimation, ColorFlowAnimation
+from ctre.led import CANdle, RainbowAnimation, ColorFlowAnimation, StrobeAnimation
 from wpilib import RobotState
 
 import constants
@@ -19,7 +19,8 @@ class LightSubsystem(SubsystemBase):
         self.setName(__class__.__name__)
         self.light = CANdle(constants.kCANdleID, constants.kCANivoreName)
 
-        self.disabledAnimation = RainbowAnimation(1, 0.5, 68)
+        self.disabledAnimation1 = RainbowAnimation(1, 0.5, 17, ledOffset=8)
+        self.disabledAnimation2 = RainbowAnimation(1, 0.5, 17, ledOffset=8 + 17)
         self.cubeAnimation1 = ColorFlowAnimation(
             204, 0, 204, 255, 0.7, 17, ledOffset=8
         )  # purple
@@ -33,14 +34,22 @@ class LightSubsystem(SubsystemBase):
             255, 255, 0, 255, 0.7, 17, ledOffset=8 + 17
         )  # yellow
 
+        self.estopAnim1 = StrobeAnimation(255, 0, 0, 255, 0.3, 17, 8)
+        self.estopAnim2 = StrobeAnimation(255, 0, 0, 255, 0.3, 17, 8 + 17)
+
         self.state = LightSubsystem.State.No
 
     def periodic(self) -> None:
         if RobotState.isDisabled():
-            self.light.animate(self.disabledAnimation)
+            self.light.animate(self.disabledAnimation1)
+            self.light.animate(self.disabledAnimation2, 1)
+        elif RobotState.isEStopped():
+            self.light.animate(self.estopAnim1)
+            self.light.animate(self.estopAnim2, 1)
         else:
             if self.state == LightSubsystem.State.No:
-                self.light.animate(self.disabledAnimation)
+                self.light.animate(self.disabledAnimation1)
+                self.light.animate(self.disabledAnimation2, 1)
             elif self.state == LightSubsystem.State.Cone:
                 self.light.animate(self.coneAnimation1)
                 self.light.animate(self.coneAnimation2, 1)
